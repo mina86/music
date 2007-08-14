437f340df71e9ae23ab0e11cf7ae43e31356b1df
mina86 <mina86>
2007-08-14 00:20:39 +0000

    + structures for cache modules added + init() takes 2 arguments -- module name and argument * some names hidden when included in a module

--- music.h
+++ music.h
@@ -22,8 +22,16 @@ struct song {
 	const char *artist;
 	const char *album;
 	const char *genre;
-	int time;
-	int length;
+	time_t time;
+	time_t endTime;
+	unsigned length;
+};
+
+
+struct cache_entry {
+	unsigned count;
+	const char **modules;
+	struct song song;
 };
 
 
@@ -35,25 +43,39 @@ struct music_option {
 
 struct module;
 
+
 struct module_functions {
+	/* both for i/o modules and cache */
 	int  (*start)(struct module *m);
 	void (*stop)(struct module *m);
-	int  (*putSong)(struct module *m, const struct song *song);
 	int  (*configLine)(struct module *m, const char *opt,
 	                   const char *arg);
 	int  (*configEnd)(struct module *m);
+	/* only for i/o modules */
+	int  (*putSong)(struct module *m, const struct song *song);
+	/* only for cache */
+	void (*addSong)(struct module *m, const struct song *song,
+	                const char *modules, ...);
+	void (*query)(struct module *m);
+	struct cache_entry *(*next)(struct module *m);
+	void (*update)(struct module *m, ... /* const char *modules */);
 };
+
 
 struct module {
 	const struct module_functions *f; /* functions */
 	char *name;                       /* if overwritten must be free()d */
-	struct module *core, *next;       /* for music's use */
+#ifdef MUSIC_NOT_MODULE
+	struct module *core, *next;       /* for internal use */
+#else
+	void *internal1, *internal2;
+#endif
 	void *data;                       /* for module's use */
 };
 
 
 
-#ifdef MUSIC_NO_MODULE
+#ifdef MUSIC_NOT_MODULE
 extern volatile int music_running;
 extern int sleep_pipe_fd;
 #else
@@ -62,11 +84,13 @@ extern const int sleep_pipe_fd;
 #endif
 
 
-#define LOG_FATAL    0
-#define LOG_ERROR    4
-#define LOG_WARNING  8
-#define LOG_NOTICE  12
-#define LOG_DEBUG   16
+enum {
+	LOG_FATAL   =  0,
+	LOG_ERROR   =  4,
+	LOG_WARNING =  8,
+	LOG_NOTICE  = 12,
+	LOG_DEBUG   = 16
+};
 
 void  music_log   (struct module *m, unsigned level, const char *fmt, ...)
 	__attribute__((format (printf, 3, 4), nonnull, visibility("default")));
@@ -86,8 +110,8 @@ int music_sleep(struct module *m, unsigned long mili)
 
 
 
-#ifndef MUSIC_NO_MODULE
-struct module *init(void)
+#ifndef MUSIC_NOT_MODULE
+struct module *init(const char *name, const char *arg)
 	__attribute__((nonnull, visibility("default")));
 #endif
 
