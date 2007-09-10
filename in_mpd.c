3ab5cf1bd25f412a5eee91d1c49f69dc3a85b08b
mina86 <mina86>
2007-09-10 17:51:01 +0000

    + header added

--- in_mpd.c
+++ in_mpd.c
@@ -1,3 +1,23 @@
+/*
+ * "Listening to" daemon MPD input module
+ * $Id: in_mpd.c,v 1.5 2007/09/10 17:51:01 mina86 Exp $
+ * Copyright (c) 2007 by Michal Nazarewicz (mina86/AT/mina86.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
