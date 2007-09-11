21d9c5eb9a44f6fa1e3aaa74abad1c940f36b0aa
mina86 <mina86>
2007-09-11 14:43:57 +0000

    + HAVE_OPENSSL_H and HAVE_ENDIAN_H macros added + __restrict__ macro added + CONFIG_H macro renamed to MINA_CONFIG_H

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /*
  * "Listening to" daemon configuration file
- * $Id: config.h,v 1.3 2007/09/10 17:51:01 mina86 Exp $
+ * $Id: config.h,v 1.4 2007/09/11 14:43:57 mina86 Exp $
  * Copyright (c) 2007 by Michal Nazarewicz (mina86/AT/mina86.com)
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,18 +18,23 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef MN_CONFIG_H
+#define MN_CONFIG_H
+
 
 #define HAVE_POLL 1
+#define HAVE_OPENSSL_H 0
+#define HAVE_ENDIAN_H 1
 
 
 #ifndef __GNUC__
 # define __attribute__(x)
 # if __STDC_VERSION__ + 0 >= 199901L
 #  define __inline__ inline
+#  define __restrict__ restrict
 # else
 #  define __inline__
+#  define __restrict__
 # endif
 #endif
 
