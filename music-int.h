29696f142dca195eb7bfbfec7d8af055096e7fdb
mina86 <mina86>
2007-09-20 03:21:28 +0000

    + song dispatcher stuff removed

--- music-int.h
+++ music-int.h
@@ -1,6 +1,6 @@
 /*
  * "Listening to" daemon internal header file
- * $Id: music-int.h,v 1.1 2007/09/19 02:32:57 mina86 Exp $
+ * $Id: music-int.h,v 1.2 2007/09/20 03:21:28 mina86 Exp $
  * Copyright (c) 2007 by Michal Nazarewicz (mina86/AT/mina86.com)
  *
  * This program is free software; you can redistribute it and/or modify
@@ -27,16 +27,6 @@
 
 
 /**
- * A linked list element.
- */
-struct slist {
-	struct slist *next; /**< Next element. */
-	void *ptr;          /**< Data pointer. */
-};
-
-
-
-/**
  * A core module configuration structure.
  */
 struct config {
@@ -46,16 +36,6 @@ struct config {
 	 * starting in the middle of another message.
 	 */
 	pthread_mutex_t log_mutex;
-
-	/**
-	 * Configuration for songs disptcher.
-	 */
-	struct music_config_songs {
-		pthread_t dispatcher;   /**< Song dispatcher's thread ID/ */
-		pthread_mutex_t mutex;  /**< Mutex used by song dispatcher. */
-		pthread_cond_t cond;    /**< Song dispatcher's condition variable. */
-		struct slist *first;    /**< First song on songs queue. */
-	} songs;
 
 	char    *logfile;           /**< A log file. */
 	unsigned loglevel;          /**< A log level.  Messages with
